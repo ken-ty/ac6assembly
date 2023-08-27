29c9c26e084ec51c78285bedf47dd94bfe9ffcaa
todoggler <kentokuraCS@gmail.com>
2023-08-28 00:15:08 +0900

    refactor: mainファイルを分割

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:ac6assembly/page/search_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SearchPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
